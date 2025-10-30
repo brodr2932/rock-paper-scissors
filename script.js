edaa19fbbed2e333fc98e91353dd2a954f55cd73
Brandon Rodriguez <brandon2932@gmail.com>
2025-10-30 16:33:31 -0700

    Write logic to play single round

--- script.js
+++ script.js
@@ -23,3 +23,27 @@ function getHumanChoice() {
 let humanScore = 0;
 let computerScore = 0;
 
+function playRound(humanChoice, computerChoice) {
+  console.log(`human's choice: ${humanChoice}. Computer's choice: ${computerChoice}`);
+
+  if (humanChoice === computerChoice) {
+    console.log("It's a draw!");
+    return;
+  }
+
+  if (
+    humanChoice === "rock" && computerChoice === "scissors" ||
+    humanChoice === "paper" && computerChoice === "rock" ||
+    humanChoice === "scissors" && computerChoice === "paper"
+  ) {
+    console.log("You win!");
+    humanScore++;
+  } else {
+    console.log("You lose!");
+    computerScore++;
+  }
+}
+
+playRound(getHumanChoice(), getComputerChoice());
+
+
