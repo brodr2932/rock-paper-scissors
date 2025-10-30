9e6fdc383ac4055a91b9c2d29fa35f310c8494b1
Brandon Rodriguez <brandon2932@gmail.com>
2025-10-30 15:00:56 -0700

    Write logic to get computer choice

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log("Hello World")
+function getComputerChoice() {
+  const randomVal = Math.random();
+
+  if (randomVal < 0.3) {
+    return "rock";
+  } else if (randomVal < 0.6) {
+    return "paper";
+  } else {
+    return "scissors";
+  }
+}
+
+console.log(getComputerChoice());
